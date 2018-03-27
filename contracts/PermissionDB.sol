282ddb65547d8059b5cc5b0c5bb313c8a6df560a
Luis Mata Salvatierra <luis.mata.salvatierra@alumnos.upm.es>
2018-03-27 17:20:35 +0200

    Refactor Database Contract

--- contracts/PermissionDB.sol
+++ contracts/PermissionDB.sol
@@ -1,10 +1,9 @@
 pragma solidity ^0.4.4;
 
 import "./DataHelper.sol";
-import "./DougEnabled.sol";
-import "./Doug.sol";
+import "./Database.sol";
 
-contract PermissionDB is DougEnabled{
+contract PermissionDB is Database{
     struct Permit {
         address agency;
         address owner;
@@ -14,24 +13,18 @@ contract PermissionDB is DougEnabled{
 
     mapping (bytes32 => Permit) permissions;
 
-    modifier senderIsController() {
-        address _contractAddress = Doug(DOUG).getContract("perm");
-        require (msg.sender == _contractAddress);
-        _;
-    }
-
-    function setPermission(bytes32 policyNumber, address _agency, address _owner) senderIsController public {
+    function setPermission(bytes32 policyNumber, address _agency, address _owner) senderIsController("perm") public {
         Permit storage permit = permissions[policyNumber];
         permit.agency = _agency;
         permit.owner = _owner;
         permit.numGuests = 0;
     }
 
-    function getPermission(bytes32 policyNumber) senderIsController public view returns(address, address, address[10]) {
+    function getPermission(bytes32 policyNumber) senderIsController("perm") public view returns(address, address, address[10]) {
         return (permissions[policyNumber].agency, permissions[policyNumber].owner, permissions[policyNumber].guests);
     }
 
-    function addGuest(address guest, bytes32 policyNumber) senderIsController public {
+    function addGuest(address guest, bytes32 policyNumber) senderIsController("perm") public {
         Permit storage permit = permissions[policyNumber];
         permit.guests[permit.numGuests] =  guest;
         permit.numGuests++;
--- contracts/PolicyDB.sol
+++ contracts/PolicyDB.sol
@@ -1,62 +1,54 @@
 pragma solidity ^0.4.4;
 
-import "./DougEnabled.sol";
-import "./Doug.sol";
+import "./Database.sol";
 import "./DataHelper.sol";
 import "./DateTime.sol";
 
-contract PolicyDB is DougEnabled{
-  uint numPolicies;
-  mapping (uint => DataHelper.Policy) policies;
+contract PolicyDB is Database {
+    uint numPolicies;
+    mapping (uint => DataHelper.Policy) policies;
 
-  modifier senderIsController() {
-      address _contractAddress = Doug(DOUG).getContract("policy");
-      require (msg.sender == _contractAddress);
-      _;
-  }
+    function createPolicy(
+        uint _ownerId,
+        bytes32 _name,
+        uint _effectiveDate,
+        uint _expirationDate
+    )
+        senderIsController("policy") public returns (uint id)
+    {
+        numPolicies++;
+        DataHelper.Policy storage policy = policies[numPolicies];
+        policy.policyNumber = numPolicies;
+        policy.ownerId = _ownerId;
+        policy.name = _name;
+        policy.status = DataHelper.Stage.Active;
+        policy.effectiveDate = _effectiveDate;
+        policy.expirationDate = _expirationDate;
+        return numPolicies;
+    }
 
-  function createPolicy(
-      uint _ownerId,
-      bytes32 _name,
-      uint _effectiveDate,
-      uint _expirationDate
-  )
-      senderIsController public returns (uint id)
-  {
-      numPolicies++;
-      DataHelper.Policy storage policy = policies[numPolicies];
-      policy.policyNumber = numPolicies;
-      policy.ownerId = _ownerId;
-      policy.name = _name;
-      policy.status = DataHelper.Stage.Active;
-      policy.effectiveDate = _effectiveDate;
-      policy.expirationDate = _expirationDate;
-      return numPolicies;
-  }
+    function getPolicy(uint _policyNumber) senderIsController("policy") public view
+        returns(uint, uint, bytes32, DataHelper.Stage, uint, uint)
+    {
+        DataHelper.Policy storage policy = policies[_policyNumber];
+        return (policy.policyNumber, policy.ownerId, policy.name, policy.status, policy.effectiveDate, policy.expirationDate);
+    }
 
-  function getPolicy(uint _policyNumber) senderIsController public view
-      returns(uint, uint, bytes32, DataHelper.Stage, uint, uint)
-  {
-      DataHelper.Policy storage policy = policies[_policyNumber];
-      return (policy.policyNumber, policy.ownerId, policy.name, policy.status, policy.effectiveDate, policy.expirationDate);
-  }
+    function updateStatus(uint policyNumber, DataHelper.Stage _status) senderIsController("policy") public {
+        policies[policyNumber].status = _status;
+    }
 
-  function updateStatus(uint policyNumber, DataHelper.Stage _status) senderIsController public {
-      policies[policyNumber].status = _status;
-  }
-
-  function changeToExpired() senderIsController public {
-      uint _expirationDate;
-      for(uint i = 0; i <= numPolicies; i++) {
-          _expirationDate = policies[i].expirationDate;
-          if(
-              DateTime.getYear(now) >= DateTime.getYear(_expirationDate) &&
-              DateTime.getMonth(now) >= DateTime.getMonth(_expirationDate) &&
-              DateTime.getDay(now) > DateTime.getDay(_expirationDate)
-          ) {
-
-                  policies[i].status = DataHelper.Stage.Expired;
-          }
-      }
-  }
+    function changeToExpired() senderIsController("policy") public {
+        uint _expirationDate;
+        for(uint i = 0; i <= numPolicies; i++) {
+            _expirationDate = policies[i].expirationDate;
+            if(
+                DateTime.getYear(now) >= DateTime.getYear(_expirationDate) &&
+                DateTime.getMonth(now) >= DateTime.getMonth(_expirationDate) &&
+                DateTime.getDay(now) > DateTime.getDay(_expirationDate)
+            ) {
+                    policies[i].status = DataHelper.Stage.Expired;
+            }
+        }
+    }
 }
