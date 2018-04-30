f2fcb14a32683f7c8e11399ae3a62fb8e7bfcf58
Luis Mata Salvatierra <luis.mata.salvatierra@alumnos.upm.es>
2018-04-30 12:01:38 +0200

    Update sandbox data script to use CreatePolicy new interface

--- scripts/add_sandbox_data.js
+++ scripts/add_sandbox_data.js
@@ -11,9 +11,9 @@ module.exports = async function(callback) {
         await manager.createBroker(web3.fromAscii("TestCreation@Broker.com"), "admin", web3.fromAscii("Coverwallet"), web3.fromAscii("2128677475"), web3.fromAscii("Alcala 21"));
         await manager.createCoi("CertificateTest@cosa.com");
         await manager.createOwner(web3.fromAscii("Hola@cosa.com"), "admin", web3.fromAscii("cosa"), web3.fromAscii("Alcala 21"));
-        await manager.createPolicy(1, web3.fromAscii("Workers Comp"), timeNow, oneYearFromNow, 1);
+        await manager.createPolicy(web3.fromAscii("CertificateTest@cosa.com"), web3.fromAscii("Workers Comp"), timeNow, oneYearFromNow, 1);
         await manager.addPolicy(1, 1)
-        await manager.createPolicy(1, web3.fromAscii("Business Owners Policy"), timeNow, oneYearFromNow, 1);
+        await manager.createPolicy(web3.fromAscii("CertificateTest@cosa.com"), web3.fromAscii("Business Owners Policy"), timeNow, oneYearFromNow, 1);
         await manager.addPolicy(1, 2)
     } catch(e) {
         console.error(e);
