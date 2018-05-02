309e4d5457ec3d15772b73c1d60d89dbe96f9ef4
Luis Mata Salvatierra <luis.mata.salvatierra@alumnos.upm.es>
2018-05-02 17:18:30 +0200

    Add method to get policies of carrier

--- contracts/managers/PolicyManager.sol
+++ contracts/managers/PolicyManager.sol
@@ -104,4 +104,51 @@ contract PolicyManager {
         address policy = obtainControllerContract("policy");
         Policy(policy).changePolicyToExpired();
     }
+
+    function getPoliciesOfCarrier(uint carrierId) public view returns (string json) {
+        address policy = obtainControllerContract("policy");
+        strings.slice[] memory objects = new strings.slice[](20);
+        uint numPolicies = Policy(policy).getNumPolicies();
+        uint last;
+        for(uint i = 1; i <= numPolicies; i++) {
+            if(Policy(policy).isPolicyOfCarrier(i, carrierId)){
+                objects[i*2] = getSummaryOfPolicy(i).toSlice();
+                objects[(i*2)+1] = ",".toSlice();
+                last = (i*2)+1;
+            }
+        }
+        objects[last] = "".toSlice();
+        json = wrapObjectInArray("".toSlice().join(objects));
+    }
+
+    function getSummaryOfPolicy(uint policyNumber)
+    public view
+    returns(string policyString)
+    {
+        address policy = obtainControllerContract("policy");
+        address user = obtainControllerContract("user");
+        bytes32 _userName;
+        uint _policyNumber;
+        uint _ownerId;
+        bytes32 _name;
+        DataHelper.Stage _status;
+        uint _effectiveDate;
+        uint _expirationDate;
+        (_policyNumber,
+        _ownerId,
+        _name,
+        _status,
+        _effectiveDate,
+        _expirationDate,
+        ) = Policy(policy).getPolicy(policyNumber);
+        (_userName, ,) = User(user).getOwner(_ownerId);
+        strings.slice[] memory items = new strings.slice[](6);
+        items[0] = itemJson("policy_number", stringsUtil.uintToString(_policyNumber), false);
+        items[1] = itemJson("user_email", stringsUtil.bytes32ToString(_userName), false);
+        items[2] = itemJson("insurance_type", stringsUtil.bytes32ToString(_name), false);
+        items[3] = itemJson("status", stringsUtil.uintToString(uint(_status)), false);
+        items[4] = itemJson("effective_date", stringsUtil.uintToString(_effectiveDate), false);
+        items[5] = itemJson("expiration_date", stringsUtil.uintToString(_expirationDate), true);
+        policyString = wrapJsonObject("".toSlice().join(items));
+    }
 }
