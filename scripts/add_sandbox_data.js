9e43895bdc6f5f68763cdad80e6de6a2aa1b6851
Luis Mata Salvatierra <luis.mata.salvatierra@alumnos.upm.es>
2018-05-08 15:43:43 +0200

    Fix typo in sandbox data script

--- scripts/add_sandbox_data.js
+++ scripts/add_sandbox_data.js
@@ -5,15 +5,15 @@ module.exports = async function(callback) {
     let timeNow = Math.floor(Date.now() / 1000);
     let oneYearFromNow = timeNow + 31556926;
     try {
-        await manager.createOwner(web3.fromAscii("Test5@Owner.com"), "admin", web3.fromAscii("Luis Miguel"), web3.fromAscii("Alcala 21"));
+        await manager.createOwner(web3.fromAscii("Test1@Owner.com"), "admin", web3.fromAscii("Luis Miguel"), web3.fromAscii("Alcala 21"));
         await manager.createCarrier(web3.fromAscii("Test2@Carrier.com"), "admin", web3.fromAscii("CNA"));
         await manager.createOwner(web3.fromAscii("Test3@Owner.com"), "admin", web3.fromAscii("Roberto Carlos"), web3.fromAscii("Alcala 21"));
         await manager.createBroker(web3.fromAscii("Test4@Broker.com"), "admin", web3.fromAscii("Coverwallet"), web3.fromAscii("2128677475"), web3.fromAscii("Alcala 21"));
         await manager.createOwner(web3.fromAscii("Test5@Owner.com"), "admin", web3.fromAscii("Pedro Pablo"), web3.fromAscii("Alcala 21"));
         await manager.createPolicy(web3.fromAscii("Test5@Owner.com"), web3.fromAscii("Business Owners Policy"), timeNow, oneYearFromNow, 2);
         await manager.createPolicy(web3.fromAscii("Test5@Owner.com"), web3.fromAscii("General Liability"), timeNow, oneYearFromNow, 2);
-        await manager.createPolicy(web3.fromAscii("Test5@Owner.com"), web3.fromAscii("Workers Compensation"), timeNow, oneYearFromNow, 2);
-        await manager.createPolicy(web3.fromAscii("Test5@Owner.com"), web3.fromAscii("Business Owners Policy"), timeNow, oneYearFromNow, 2);
+        await manager.createPolicy(web3.fromAscii("Test1@Owner.com"), web3.fromAscii("Workers Compensation"), timeNow, oneYearFromNow, 2);
+        await manager.createPolicy(web3.fromAscii("Test1@Owner.com"), web3.fromAscii("Business Owners Policy"), timeNow, oneYearFromNow, 2);
         await manager.createCoi("Test5@Owner.com", timeNow, 4, [1, 2]);
     } catch(e) {
         console.error(e);
