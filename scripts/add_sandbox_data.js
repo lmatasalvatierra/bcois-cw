60c2273d4232a2635623989841cbaff7fd6ab85f
Luis Mata Salvatierra <luis.mata.salvatierra@alumnos.upm.es>
2018-05-08 15:30:50 +0200

    Change data of sandbox information added to blockchain

--- scripts/add_sandbox_data.js
+++ scripts/add_sandbox_data.js
@@ -5,16 +5,16 @@ module.exports = async function(callback) {
     let timeNow = Math.floor(Date.now() / 1000);
     let oneYearFromNow = timeNow + 31556926;
     try {
-        await manager.createOwner(web3.fromAscii("Test@Owner.com"), "admin", web3.fromAscii("DY"), web3.fromAscii("Alcala 21"));
-        await manager.createCarrier(web3.fromAscii("TestCreation@Carrier.com"), "admin", web3.fromAscii("CNA"));
-        await manager.createOwner(web3.fromAscii("CertificateTest@cosa.com"), "admin", web3.fromAscii("cosa"), web3.fromAscii("Alcala 21"));
-        await manager.createBroker(web3.fromAscii("TestCreation@Broker.com"), "admin", web3.fromAscii("Coverwallet"), web3.fromAscii("2128677475"), web3.fromAscii("Alcala 21"));
-        await manager.createOwner(web3.fromAscii("Hola@cosa.com"), "admin", web3.fromAscii("cosa"), web3.fromAscii("Alcala 21"));
-        await manager.createPolicy(web3.fromAscii("Hola@cosa.com"), web3.fromAscii("BOP"), timeNow, oneYearFromNow, 2);
-        await manager.createPolicy(web3.fromAscii("Hola@cosa.com"), web3.fromAscii("GEneral Liability"), timeNow, oneYearFromNow, 2);
-        await manager.createPolicy(web3.fromAscii("CertificateTest@cosa.com"), web3.fromAscii("Workers Comp"), timeNow, oneYearFromNow, 2);
-        await manager.createPolicy(web3.fromAscii("CertificateTest@cosa.com"), web3.fromAscii("Business Owners Policy"), timeNow, oneYearFromNow, 2);
-        await manager.createCoi("Hola@cosa.com", timeNow, 4, [1, 2]);
+        await manager.createOwner(web3.fromAscii("Test5@Owner.com"), "admin", web3.fromAscii("Luis Miguel"), web3.fromAscii("Alcala 21"));
+        await manager.createCarrier(web3.fromAscii("Test2@Carrier.com"), "admin", web3.fromAscii("CNA"));
+        await manager.createOwner(web3.fromAscii("Test3@Owner.com"), "admin", web3.fromAscii("Roberto Carlos"), web3.fromAscii("Alcala 21"));
+        await manager.createBroker(web3.fromAscii("Test4@Broker.com"), "admin", web3.fromAscii("Coverwallet"), web3.fromAscii("2128677475"), web3.fromAscii("Alcala 21"));
+        await manager.createOwner(web3.fromAscii("Test5@Owner.com"), "admin", web3.fromAscii("Pedro Pablo"), web3.fromAscii("Alcala 21"));
+        await manager.createPolicy(web3.fromAscii("Test5@Owner.com"), web3.fromAscii("Business Owners Policy"), timeNow, oneYearFromNow, 2);
+        await manager.createPolicy(web3.fromAscii("Test5@Owner.com"), web3.fromAscii("General Liability"), timeNow, oneYearFromNow, 2);
+        await manager.createPolicy(web3.fromAscii("Test5@Owner.com"), web3.fromAscii("Workers Compensation"), timeNow, oneYearFromNow, 2);
+        await manager.createPolicy(web3.fromAscii("Test5@Owner.com"), web3.fromAscii("Business Owners Policy"), timeNow, oneYearFromNow, 2);
+        await manager.createCoi("Test5@Owner.com", timeNow, 4, [1, 2]);
     } catch(e) {
         console.error(e);
     }
