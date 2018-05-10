db9366c276c6ec2e00a403eed7eb096f9993511c
Luis Mata Salvatierra <luis.mata.salvatierra@alumnos.upm.es>
2018-05-10 12:31:27 +0200

    Update sandbox data adding uuid to policy

--- scripts/add_sandbox_data.js
+++ scripts/add_sandbox_data.js
@@ -1,4 +1,6 @@
 var COIManager = artifacts.require("./COIManager.sol");
+const uuidv4 = require('uuid/v4');
+const uuidToHex = require('uuid-to-hex');
 
 module.exports = async function(callback) {
     var manager = await COIManager.deployed();
@@ -10,10 +12,10 @@ module.exports = async function(callback) {
         await manager.createOwner(web3.fromAscii("Test3@Owner.com"), "admin", web3.fromAscii("Roberto Carlos"), web3.fromAscii("Alcala 21"));
         await manager.createBroker(web3.fromAscii("Test4@Broker.com"), "admin", web3.fromAscii("Coverwallet"), web3.fromAscii("2128677475"), web3.fromAscii("Alcala 21"));
         await manager.createOwner(web3.fromAscii("Test5@Owner.com"), "admin", web3.fromAscii("Pedro Pablo"), web3.fromAscii("Alcala 21"));
-        await manager.createPolicy(web3.fromAscii("Test5@Owner.com"), web3.fromAscii("Business Owners Policy"), timeNow, oneYearFromNow, 2);
-        await manager.createPolicy(web3.fromAscii("Test5@Owner.com"), web3.fromAscii("General Liability"), timeNow, oneYearFromNow, 2);
-        await manager.createPolicy(web3.fromAscii("Test1@Owner.com"), web3.fromAscii("Workers Compensation"), timeNow, oneYearFromNow, 2);
-        await manager.createPolicy(web3.fromAscii("Test1@Owner.com"), web3.fromAscii("Business Owners Policy"), timeNow, oneYearFromNow, 2);
+        await manager.createPolicy(web3.fromAscii("Test5@Owner.com"), web3.fromAscii("Business Owners Policy"), timeNow, oneYearFromNow, 2, uuidToHex(uuidv4(), true));
+        await manager.createPolicy(web3.fromAscii("Test5@Owner.com"), web3.fromAscii("General Liability"), timeNow, oneYearFromNow, 2, uuidToHex(uuidv4(), true));
+        await manager.createPolicy(web3.fromAscii("Test1@Owner.com"), web3.fromAscii("Workers Compensation"), timeNow, oneYearFromNow, 2, uuidToHex(uuidv4(), true));
+        await manager.createPolicy(web3.fromAscii("Test1@Owner.com"), web3.fromAscii("Business Owners Policy"), timeNow, oneYearFromNow, 2, uuidToHex(uuidv4(), true));
         await manager.createCoi("Test5@Owner.com", timeNow, 4, [1, 2]);
     } catch(e) {
         console.error(e);
